e534a4629860970ba19baf12f185527fca73fa23
gkumar-pandey <gautamshekhar078@gmail.com>
2022-08-18 08:44:59 +0530

    add check validation of inputs in registration page

--- src/Components/RegisterPage/Register.js
+++ src/Components/RegisterPage/Register.js
@@ -1,36 +1,132 @@
-import React from 'react'
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Button,message } from 'antd';
-import { Input, Space } from 'antd';
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Button, message } from "antd";
+import { Input, Space } from "antd";
 function Register() {
-  
-  const register = ()=> {
-    message.info('Register btn clicked')
-  }
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
 
+  const valid = (userName, password, confirmPassword) => {
+    let isValid = true;
+    if (userName !== "" && isValid) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error("username is required");
+      return;
+    }
+
+    if (userName.length >= 6 && userName.length <= 32 && isValid) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error(
+        "Username should contain atleast 6 character and most 32 character"
+      );
+    }
+    if (password !== "" && isValid) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error("password is required");
+      return;
+    }
+
+    if (password.length >= 6 && password.length <= 32 && isValid) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error(
+        "Password should contain atleast 6 character and most 32 character"
+      );
+      return;
+    }
+    if (confirmPassword !== "" && isValid) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error("confirmPassword is required");
+      return;
+    }
+
+    if (
+      confirmPassword.length >= 6 &&
+      confirmPassword.length <= 32 &&
+      isValid
+    ) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error(
+        "confirmPassword should contain atleast 6 character and most 32 character"
+      );
+      return;
+    }
+
+    if (password == confirmPassword) {
+      isValid = true;
+    } else {
+      isValid = false;
+      message.error("Password and Confirm password not matching");
+      return;
+    }
+    return isValid;
+  };
+
+  const registerBtnClickHandle = () => {
+    const allRegistrationInputsAreValid = valid(
+      userName,
+      password,
+      confirmPassword
+    );
+  };
 
   return (
     <>
-      <Header/>
+      <Header />
       <div className="flex-container">
-      
         <div className="container">
           <h1>Make an account</h1>
-          <Space direction='vertical' style={{width: '80%'}}>
-            <Input placeholder="Username" maxLength='5' prefix={<UserOutlined />} />
-            <Input.Password placeholder=" Password" prefix={<LockOutlined />} />
-            <Input.Password placeholder="Confirm password" prefix={<LockOutlined />} />
+          <Space direction="vertical" style={{ width: "80%" }}>
+            <Input
+              placeholder="Username"
+              maxLength="32"
+              prefix={<UserOutlined />}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <Input.Password
+              placeholder=" Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              prefix={<LockOutlined />}
+            />
+            <Input.Password
+              placeholder="Confirm password"
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+              prefix={<LockOutlined />}
+            />
           </Space>
 
-          <Button onClick={register} className='btn-block' type='primary'>Register</Button>
-
+          <Button
+            onClick={registerBtnClickHandle}
+            className="btn-block"
+            type="primary"
+          >
+            Register
+          </Button>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
