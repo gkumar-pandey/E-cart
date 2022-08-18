e07d2a98313488c8fe495f5f3121eb7cd741be23
gkumar-pandey <gautamshekhar078@gmail.com>
2022-08-18 09:28:16 +0530

    add input valid check function in login page

--- src/Components/LoginPage/Login.js
+++ src/Components/LoginPage/Login.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/Header";
-import Footer from '../Footer/Footer';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Button ,message} from 'antd';
-import { Input, Space } from 'antd';
+import Footer from "../Footer/Footer";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Button, message } from "antd";
+import { Input, Space } from "antd";
 function Login() {
+  const [loginUserName, setLoginUserName] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
-  const loginMsg = () => {
-    message.info('Login is clicked')
-  }
+  // ! Check validation of Login input values
+  //? if userName is empty return false else true
+  //? if password is empty return false else true
+
+  const isValid = (userLoginName, userLoginPassword) => {
+    let valid = true;
+    if (userLoginName !== "" && valid) {
+      valid = true;
+    } else {
+      valid = false;
+      message.error("Enter User name");
+      return;
+    }
+
+    if (userLoginPassword !== "" && valid) {
+      valid = true;
+    } else {
+      valid = false;
+      message.error("Enter Password");
+      return;
+    }
+    return valid;
+  };
+
+  const handleLoginClickBtn = () => {
+    console.log(isValid(loginUserName, loginPassword));
+  };
 
   return (
     <>
@@ -19,14 +45,22 @@ function Login() {
           <Space direction="vertical" style={{ width: "80%" }}>
             <Input
               placeholder="Username"
-              maxLength="5"
+              maxLength="32"
               prefix={<UserOutlined />}
+              onChange={(e) => setLoginUserName(e.target.value)}
             />
-            <Input.Password placeholder=" Password" prefix={<LockOutlined />} />
-             
+            <Input.Password
+              placeholder=" Password"
+              prefix={<LockOutlined />}
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
           </Space>
 
-          <Button onClick={loginMsg} className="btn-block" type="primary">
+          <Button
+            onClick={handleLoginClickBtn}
+            className="btn-block"
+            type="primary"
+          >
             Login
           </Button>
         </div>
