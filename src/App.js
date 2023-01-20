6270743c061723ef8c7298d2dba6c9ee0a8c1e57
gkumar-pandey <gautamshekhar078@gmail.com>
2023-01-20 11:40:09 +0530

    refactor: update the code

--- src/App.js
+++ src/App.js
@@ -5,27 +5,40 @@ import Login from "./Components/LoginPage/Login";
 import "antd/dist/antd.css";
 import CheckoutPage from "./Components/Checkout/CheckoutPage";
 import Thanks from "./Components/Thanks/Thanks";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
 import ProductPage from "./Components/Search/ProductPage";
+import RegisterPage from "./Pages/AuthPage/RegisterPage/RegisterPage";
 import { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import LoginPage from "./Pages/AuthPage/LoginPage/LoginPage";
+import { Toaster } from "react-hot-toast";
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { pathname } = useLocation();
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/products"
-            element={<ProductPage isLoggedIn={isLoggedIn} />}
-          />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="ordered" element={<Thanks />} />
-        </Routes>
-      </Router>
+    <div>
+      <Toaster />
+      {pathname == "/" || pathname == "/register" || pathname == "/login" ? (
+        <Navbar />
+      ) : null}
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route
+          path="/products"
+          element={<ProductPage isLoggedIn={isLoggedIn} />}
+        />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="ordered" element={<Thanks />} /> */}
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CartContext from "./Components/CartContext/CartContext";
+import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
 
+// Call make Server
+makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CartContext>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </CartContext>
   </React.StrictMode>
 );
