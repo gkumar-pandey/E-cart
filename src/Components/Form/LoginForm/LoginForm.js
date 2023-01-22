0b0d9393ca71eb8164990058dc56b7d22c7b6364
gkumar-pandey <gautamshekhar078@gmail.com>
2023-01-22 22:41:08 +0530

    feat: login form created

--- src/Components/Form/LoginForm/LoginForm.js
+++ src/Components/Form/LoginForm/LoginForm.js
@@ -3,37 +3,63 @@ import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { Button, Form } from "antd";
 import { Input } from "antd";
 import Label from "./Components/Label";
+import { Checkbox } from "antd";
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    emailValidationStatus,
+    passwordValidationStatus,
+    checkboxHandler,
+    saveUser
+  } = props;
+
   return (
     <>
       <div className="form-input-container">
         <Form layout="vertical">
           <Form.Item
             label={<Label title={"Email"} />}
-            name="Email"
-            validateStatus="error"
-            help="Should be combination of numbers & alphabets"
-            rules={[
-              {
-                required: true
-              }
-            ]}
+            validateStatus={
+              emailValidationStatus.status && emailValidationStatus.status
+            }
+            help={emailValidationStatus.msg && emailValidationStatus.msg}
           >
-            <Input placeholder="abc@gmail.com" prefix={<UserOutlined />} />
+            <Input
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="abc@gmail.com"
+              value={email}
+              prefix={<UserOutlined />}
+            />
           </Form.Item>
+
           <Form.Item
             label={<Label title={"Password"} />}
-            name="Password"
-            validateStatus="error"
-            help="Password .."
+            validateStatus={
+              passwordValidationStatus.status && passwordValidationStatus.status
+            }
+            help={passwordValidationStatus.msg && passwordValidationStatus.msg}
             rules={[
               {
                 required: true
               }
             ]}
           >
-            <Input.Password placeholder="Password" prefix={<LockOutlined />} />
+            <Input.Password
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              value={password}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Checkbox checked={saveUser} onChange={checkboxHandler}>
+              Remember me
+            </Checkbox>
           </Form.Item>
         </Form>
       </div>
