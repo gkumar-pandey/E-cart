b34914c594fc4ff73ce9d51de379a8864aa6d4ae
gkumar-pandey <gautamshekhar078@gmail.com>
2023-01-26 22:57:14 +0530

    feat: drawer navlinks added and search bar added

--- src/Components/Navbar/Components/DrawerComp.jsx
+++ src/Components/Navbar/Components/DrawerComp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Drawer } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
 
 const DrawerTitle = () => {
   return (
@@ -9,13 +10,16 @@ const DrawerTitle = () => {
         <div style={coustemStyle.userLogo}>
           <UserOutlined style={{ fontSize: "1.5rem" }} />
         </div>
-        <span>Gautam kumar</span>
+        <span style={{ fontWeight: "bold" }}>Gautam kumar</span>
       </div>
     </>
   );
 };
 
 const DrawerComp = ({ openDrawer, handleClose }) => {
+  let activeStyle = {
+    backgroundColor: "grey"
+  };
   return (
     <>
       <Drawer
@@ -25,11 +29,39 @@ const DrawerComp = ({ openDrawer, handleClose }) => {
         onClose={handleClose}
         closable={false}
         visible={openDrawer}
-        headerStyle={{ backgroundColor: "blue" }}
+        headerStyle={coustemStyle.headerstyle}
+        bodyStyle={{ padding: "5px" }}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <NavLink
+          to=""
+          id="drawer-btn"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="profile"
+          id="drawer-btn"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Profile
+        </NavLink>
+
+        <NavLink
+          to="orders"
+          id="drawer-btn"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Orders
+        </NavLink>
+
+        <NavLink
+          to="login"
+          id="drawer-btn"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Login
+        </NavLink>
       </Drawer>
     </>
   );
@@ -39,14 +71,17 @@ export default DrawerComp;
 
 const coustemStyle = {
   userLogo: {
-    border: "4px solid red",
+    border: "4px solid white",
     borderRadius: "50%",
     padding: "0.5rem"
   },
   title: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "space-around"
-    // backgroundColor: "yellow"
+    justifyContent: "space-around",
+    color: "white"
+  },
+  headerstyle: {
+    backgroundColor: "#0A2647"
   }
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,9 +7,13 @@ import { useState } from "react";
 import Logo from "./Components/Logo";
 import WishList from "./Components/WishList";
 import "./Navbar.css";
+import SearchBar from "./Components/SearchBar";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+
+  const pathName = useLocation();
 
   const handleOpen = () => {
     setOpen(!open);
@@ -29,6 +33,14 @@ const Navbar = () => {
             <button>Home</button>
             <button>Shop now</button>
           </div>
+          {pathName == "products" ? (
+            <>
+              <div className="search-bar">
+                <SearchBar />
+              </div>
+            </>
+          ) : null}
+
           <div className="right-nav">
             <AvatarComp />
             <WishList />
