12331f4d5dfa17820a2c5554fe00c2008d951191
gkumar-pandey <gautamshekhar078@gmail.com>
2023-01-28 15:52:05 +0530

    feat: change categories and added images

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,22 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "Wireless",
+    img: "https://res.cloudinary.com/dlykup1dh/image/upload/v1674900880/e-cart/earphones_b_1_fktype.webp"
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryName: "Speakers",
+    img: "https://res.cloudinary.com/dlykup1dh/image/upload/v1674900795/e-cart/speaker3_jqagje.webp"
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "Smart watch",
+    img: "https://res.cloudinary.com/dlykup1dh/image/upload/v1674900795/e-cart/speaker3_jqagje.webp"
   },
+  {
+    _id: uuid(),
+    categories: "Wired",
+    img: "https://res.cloudinary.com/dlykup1dh/image/upload/v1674901189/e-cart/earphones_a_1_m6ng1b.webp"
+  }
 ];
